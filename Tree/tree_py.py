78b3511529038b8739ce5f2c53494544cf38de3c
cadiente-jomel <cadientejomel0@gmail.com>
2021-02-04 20:39:16 +0800

    added print_per_level method inside Tree class.

--- Tree/tree_py.py
+++ Tree/tree_py.py
@@ -95,6 +95,18 @@ class TreeNode:
         for child in self.children:
             child.print_tree(data)
 
+    def print_per_level(self, level):
+        if self.get_level() > level:
+            return
+
+        spaces = ' ' * self.get_level() * 3
+        prefix = spaces + '|____' if self.parent else ''
+
+        print(prefix + self.data)
+        if self.children:
+            for child in self.children:
+                child.print_per_level(level)
+
 
 def build_product_tree():
     ceo = TreeNode('Nilupol (CEO)')
@@ -119,5 +131,6 @@ def build_product_tree():
 if __name__ == '__main__':
     root = build_product_tree()
 
-    root.print_tree('designation')
+    # root.print_tree('designation')
+    root.print_per_level(1)
     pass
